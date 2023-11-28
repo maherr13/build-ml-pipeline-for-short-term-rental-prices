55778e3d2bf91f48974bbf9bb7936641e9f80792
maherr13 <a.maher0013@gmail.com>
2023-11-28 14:49:10 +0200

    first release

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -27,8 +27,7 @@ def go(args):
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
     
-    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
-    df = df[idx].copy()
+
 
     filename = "clean_data.csv"
     df.to_csv(filename, index=False)
